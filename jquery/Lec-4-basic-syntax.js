505351104995077ac9841d956ffa5c333fbcada7
Hafsa <hafsajabeen47@gmail.com>
2024-12-17 00:10:20 +0500

    jquery lecture notes by bhya

---
+++ jquery/Lec-4-basic-syntax.js
@@ -0,0 +1,7 @@
+// Video: 04 jQuery basic syntax:
+// jquery k code ko start krny k lye hamen aik script likhni prti hai. like this:
+
+// <script>
+//  $(document).ready(function(){
+//  write further jQuery all code here
+// });
---
+++ jquery/main.js
@@ -0,0 +1,71 @@
+// INTRODUCTION OF JQUERY:               ~~~ video: 01 ~~~
+// jQuery is a lightweight javascript library.
+// Motive for jQuery: "write less, do more"
+
+
+//@@ 1: Why use jQUERY...??
+// short selectors
+// varitey of animation functions
+// easy DOM manipulation
+// easy css styling
+// easy DOM Traversing.....mean target to dom element
+// simple ajax code
+
+
+//@@ 2: Short Selector Example:
+// document.getElementByClassName('classname');  ..... JavaScript
+// document.getElementByClassName('idname');  ..... JavaScript
+// document.getElementByClassName('tagname');  ..... JavaScript
+// $('.classname'); ..... jQuery
+// $('#idname'); ..... jQuery
+// $('tagname'); ..... jQuery
+
+
+
+//@@ 3: Benefits of jQuery:
+// browser independent ....... it works on any browser like chrome, firfox and all that.
+// increase coding speed
+
+
+
+//@@ jQuery implementation in HTML file:         ~~~ video: 02 ~~~
+// there are 3 steps to implement jquery in HTML:
+// one is download jquery.js file
+// two is include jquery.js file in HTML file
+// third is do jquery code in <script> tag
+
+// is me sab se pehly jquery.com iski site pe janahai then wahan download pe right click kr k link ko save link as krna hai. us k bad jis folder me add krni ho wahan pe link paste krna hai ya ap name change b kr skty hen. phr html ki file k head tag me script tag use kr k us me scr"" me us file ka name likhna hai jo hum ne link download krty hvy save link as kia tha. then html me body tag ki closing se pehly script tag lga kr us me code ko test krny k lye koi code likha tha k hmari jquery ki file work kr rahi hai ya nahi.
+
+
+
+
+//@@ jQuery Implementation with CDN Path:              ~~~ video: 03 ~~~
+// CDN stands for: Content Delivery Network
+// jquery.com pe jana download pe click krna hai us k bad cdn waly section pe jquery.com ka link us pe click krna hai us k bad minified yani compressed file ko open krna hai or script wala code copy krna hai phr us copied code ko html ki file me paste krna hai. then test krna hai k code work kr raha hai ya nahi.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function show(elemnt)
+{
+    var elm = document.querySelector(element);
+    elm.style.display = "block";
+}
+
+function hide(elemnt)
+{
+    var elm = document.querySelector(element);
+    elm.style.display = "hide";
+}
+
